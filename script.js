d77a025b73d9d6045921054c84805b9b0e2306ef
EvanNikaloasTheFirst <evangelosniko@outlook.com>
2023-10-04 23:26:57 +0100

    update 17

--- script.js
+++ script.js
@@ -32,3 +32,33 @@ if (screenWidth <= 850){
 }
 }
 
+
+const subTrainBox1 = document.querySelector('.subTrainBox1');
+const subTrainBox2 = document.querySelector('.subTrainBox2');
+const whoAreWe = document.querySelector('whoAreWe2');
+
+function monitorWindowSize() {
+    const windowWidth = window.innerWidth;
+
+    if (windowWidth > 850) {
+        subTrainBox1.style.flexDirection = 'column';
+        subTrainBox1.style.margin = '0 auto';
+        subTrainBox1.style.paddingLeft = '100px';
+        subTrainBox1.style.textAlign = 'center';
+        subTrainBox2.style.flexDirection = 'column-reverse';
+        whoAreWe.style.display = 'none';
+
+    } else {
+        // Reset styles if the window width is not greater than 850
+        subTrainBox1.style.flexDirection = ''; // Reset to default value
+        subTrainBox1.style.margin = '';
+        subTrainBox1.style.paddingLeft = '';
+        subTrainBox1.style.textAlign = '';
+        subTrainBox2.style.flexDirection = '';
+    }
+}
+
+monitorWindowSize();
+
+// Add an event listener to continuously check window size changes
+window.addEventListener('resize', monitorWindowSize);
