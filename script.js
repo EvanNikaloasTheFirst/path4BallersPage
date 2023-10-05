f8f70f9f6970f7d1858c062198555e9a705f95a1
EvanNikaloasTheFirst <evangelosniko@outlook.com>
2023-10-05 23:15:17 +0100

    update21

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Get a reference to the element with the class "nav-box"
-var navBoxElement = document.querySelector('.highlight');
-var navElement = document.querySelector('nav-container input[type="checkbox"]:checked ~ .menu-items');
+var navBoxElement = document.querySelector('.nav-box');
+var navElement = document.querySelector('.nav-container input[type="checkbox"]:checked ~ .menu-items');
 navBoxElement.addEventListener('click', function() {
     // Your code to run when the element is clicked
     alert('Nav box clicked!');
@@ -8,29 +8,36 @@ navBoxElement.addEventListener('click', function() {
     // You can replace this alert with any other JavaScript code you want to execute.
 });
 
+var check = document.querySelector(".nav-box"); 
+
+check.addEventListener('click', function() {
+    // Your code to run when the element is clicked
+    alert('Navigation menu clicked');
+ 
+    // You can replace this alert with any other JavaScript code you want to execute.
+});
+
 function toggleMenu() {
-// Toggels the Nav bar 
-monitorWindowSize();
-    var menu = document.querySelector(".line"); 
-    var nav = document.querySelector("nav"); 
-    var navList = document.querySelector(".navList"); 
-    var body = document.querySelector("body"); 
+    var menu = document.querySelector(".line");
+    var nav = document.querySelector("nav");
+    var navList = document.querySelector(".navList");
     var screenWidth = window.innerWidth;
-    const elementToRemove = document.querySelector('.hidden');
-
-if (screenWidth <= 850){
-    if (navList.style.display === "block") {
-        navList.style.display = "none"; 
-        nav.style.display = "none"; 
-        document.body.style.overflow = '';
-
-    } else {
-        navList.style.display = "block"; 
-        nav.style.display = "block"; 
-        document.body.style.overflow = "hidden"; 
+  
+    if (screenWidth < 850) {
+      if (navList.style.display === "none" || navList.style.display === "") {
+        navList.style.display = "block";
+        nav.style.display = "block";
+        document.body.style.overflow = "hidden"; // Disable scrolling
+      } else {
+        navList.style.display = "none";
+        nav.style.display = "block";
+        document.body.style.overflow = ''; // Re-enable scrolling
+      }
     }
-}
-}
+  }
+  
+
+
 
 
 const subTrainBox1 = document.querySelector('.subTrainBox1');
